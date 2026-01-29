f355a6b8c1d6457a3c61788b6a9e65a5e18b555e
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2026-01-29 10:11:58 +0100

    sentiment analyser

---
+++ sentiment_analyser/dat.py
@@ -0,0 +1,7 @@
+comments = [
+    "I love this product, it's amazing!",
+    "This is the worst experience I've ever had.",
+    "Not bad, but could be better.",
+    "Absolutely fantastic service!",
+    "I would not recommend this to anyone."
+]
